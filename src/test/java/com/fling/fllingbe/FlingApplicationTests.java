c16f137278bcbb1e50efc4fe585e7d139efbc725
leeseobin00 <leeseobin000709@gmail.com>
2024-01-04 00:13:21 +0900

    🎉[feat]: 프로젝트 초기 설정

---
+++ src/test/java/com/fling/fllingbe/FlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fling.fllingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
