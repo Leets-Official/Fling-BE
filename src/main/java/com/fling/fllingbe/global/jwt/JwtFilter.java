8bbda72db00b27434259a860c9a201c8140f9071
leeseobin00 <leeseobin000709@gmail.com>
2024-01-07 03:27:46 +0900

    ✨[feat]: JwtFilter에서 resolveToken 메소드 추가
    * HttpServletRequest 객체로부터 JWT 토큰을 추출하는 로직을 구현했습니다.

---
+++ src/main/java/com/fling/fllingbe/global/jwt/JwtFilter.java
@@ -0,0 +1,26 @@
+package com.fling.fllingbe.global.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.split(" ")[1];
+        }
+        return null;
+    }
+}
