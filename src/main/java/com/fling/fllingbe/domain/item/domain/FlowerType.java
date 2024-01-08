afa71aabd59154356233175ab5d9776b1a947b2f
rootTiket <l20078829@gmail.com>
2024-01-08 18:53:31 +0900

    :sparkles: [feat]: 꽃 종류를 저장하는 엔티티와 유저가 보유한 꽃과 개수를 저장하는 엔티티 생성

---
+++ src/main/java/com/fling/fllingbe/domain/item/domain/FlowerType.java
@@ -0,0 +1,24 @@
+package com.fling.fllingbe.domain.item.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "\"FlowerType\"")
+public class FlowerType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "flower_type_id")
+    private Long flowerTypeId;
+
+    @Column(name = "flower_name")
+    private String flowerName;
+
+    @Column(name = "price")
+    private Long price;
+}
