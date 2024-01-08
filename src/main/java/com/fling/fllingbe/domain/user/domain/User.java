3b07b0eaab201a5f3d931821a452c7dd279ca9f7
rootTiket <l20078829@gmail.com>
2024-01-08 18:14:32 +0900

    :sparkles: [feat]: User domain 생성

---
+++ src/main/java/com/fling/fllingbe/domain/user/domain/User.java
@@ -0,0 +1,47 @@
+package com.fling.fllingbe.domain.user.domain;
+
+import com.fling.fllingbe.global.shared.domain.BaseTimeEntity;
+import jakarta.annotation.PreDestroy;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "\"User\"")
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Email
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    @Column()
+    private LocalDateTime deletedAt;
+
+    @PreDestroy()
+    public void preDestroy() {
+        this.deletedAt = LocalDateTime.now();
+    }
+}
