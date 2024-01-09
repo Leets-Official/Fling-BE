801484ccc4b8823f4dd6518a1f2b6db5fecf54d9
rootTiket <l20078829@gmail.com>
2024-01-09 11:46:48 +0900

    :sparkles: [feat]: decoItem, decoType 엔티티 생성

---
+++ src/main/java/com/fling/fllingbe/domain/item/domain/DecoItem.java
@@ -0,0 +1,36 @@
+package com.fling.fllingbe.domain.item.domain;
+
+
+import com.fling.fllingbe.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class DecoItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long decoItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "deco_type_id")
+    private DecoType decoType;
+
+    @Column
+    private Boolean isUsing;
+
+    @Column
+    private Long count;
+}
