b01da737a8250f03a0b88638a08fb70314b72d33
rootTiket <l20078829@gmail.com>
2024-01-09 18:00:13 +0900

    :sparkles: [feat]: writeLetter 메서드 생성

--- src/main/java/com/fling/fllingbe/domain/flower/application/FlowerService.java
+++ src/main/java/com/fling/fllingbe/domain/flower/application/FlowerService.java
@@ -1,6 +1,8 @@
 package com.fling.fllingbe.domain.flower.application;
 
 
+import com.fling.fllingbe.domain.flower.domain.Flower;
+import com.fling.fllingbe.domain.flower.domain.FlowerRepository;
 import com.fling.fllingbe.domain.flower.dto.FlowerRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,8 +12,11 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class FlowerService {
+    final private FlowerRepository flowerRepository;
     public String writeLetter(FlowerRequest request, UUID id) {
-
+        Flower flower = new Flower().builder()
+                .letter(request.getLetter())
+                .build();
         return "test";
     }
 }
