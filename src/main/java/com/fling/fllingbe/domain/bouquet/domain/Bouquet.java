c0addc0cd65869d14a4861900122be0fc9b01336
rootTiket <l20078829@gmail.com>
2024-01-09 17:24:10 +0900

    :sparkles: [feat]: bouquet 엔티티 생성

---
+++ src/main/java/com/fling/fllingbe/domain/bouquet/domain/Bouquet.java
@@ -0,0 +1,40 @@
+package com.fling.fllingbe.domain.bouquet.domain;
+
+import com.fling.fllingbe.domain.user.domain.User;
+import com.fling.fllingbe.global.shared.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Bouquet extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long flowerId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column
+    private String ribbon;
+
+    @Column
+    private String wrapper;
+
+    @Column
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column
+    private LocalDateTime updatedAt = LocalDateTime.now();
+}
