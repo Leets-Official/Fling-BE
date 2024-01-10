4897040480ada75eb715c708ea7499dfae9fd73d
DongHoon Lee <dhl1924@naver.com>
2024-01-11 01:58:57 +0900

    :ambulance:[HOTFIX]: FlingApplicationTest 클래스의 설정을 기반으로 컨텍스트 로드

--- src/test/java/com/fling/fllingbe/FlingApplicationTests.java
+++ src/test/java/com/fling/fllingbe/FlingApplicationTests.java
@@ -3,7 +3,7 @@ package com.fling.fllingbe;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = FlingApplicationTests.class)
 class FlingApplicationTests {
 
     @Test
