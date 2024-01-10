c4e2ab7fc979855593699700e08121601f5b6301
rootTiket <l20078829@gmail.com>
2024-01-10 16:40:57 +0900

    :sparkles: [feat]: 새 꽃다발을 만드는 로직 구현

---
+++ src/main/java/com/fling/fllingbe/domain/bouquet/application/BouquetService.java
@@ -0,0 +1,27 @@
+package com.fling.fllingbe.domain.bouquet.application;
+
+
+import com.fling.fllingbe.domain.bouquet.domain.Bouquet;
+import com.fling.fllingbe.domain.bouquet.repository.BouquetRepository;
+import com.fling.fllingbe.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BouquetService {
+    final private BouquetRepository bouquetRepository;
+
+    public Bouquet createNewBouquet(User user) {
+        Bouquet receiverBouquet = bouquetRepository.findByUser(user).get();
+        Bouquet newBouquet = new Bouquet().builder()
+                .user(user)
+                .ribbon(receiverBouquet.getRibbon())
+                .wrapper(receiverBouquet.getWrapper())
+                .build();
+        bouquetRepository.save(newBouquet);
+        return newBouquet;
+    }
+}
