85725d7a1a5fe0af927805a11ba746291218450c
rootTiket <l20078829@gmail.com>
2024-01-10 16:39:12 +0900

    :sparkles: [feat]: 유저가 작성한 편지와 선택한 꽃, 편지지를 db에 저장하는 로직 및 꽃다발의 개수가 10개가 될 때 마다 새 꽃다발에 꽃을 저장하는 로직 추가

--- src/main/java/com/fling/fllingbe/domain/flower/application/FlowerService.java
+++ src/main/java/com/fling/fllingbe/domain/flower/application/FlowerService.java
@@ -1,22 +1,67 @@
 package com.fling.fllingbe.domain.flower.application;
 
 
+import com.fling.fllingbe.domain.bouquet.application.BouquetService;
+import com.fling.fllingbe.domain.bouquet.domain.Bouquet;
+import com.fling.fllingbe.domain.bouquet.repository.BouquetRepository;
 import com.fling.fllingbe.domain.flower.domain.Flower;
-import com.fling.fllingbe.domain.flower.domain.FlowerRepository;
+import com.fling.fllingbe.domain.flower.repository.FlowerRepository;
 import com.fling.fllingbe.domain.flower.dto.FlowerRequest;
+import com.fling.fllingbe.domain.item.application.CardItemService;
+import com.fling.fllingbe.domain.item.application.FlowerItemService;
+import com.fling.fllingbe.domain.item.domain.CardType;
+import com.fling.fllingbe.domain.item.domain.FlowerType;
+import com.fling.fllingbe.domain.user.domain.User;
+import com.fling.fllingbe.domain.user.exception.UserNotFoundException;
+import com.fling.fllingbe.domain.user.repository.UserRepository;
+import com.fling.fllingbe.global.error.exception.ServiceException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class FlowerService {
     final private FlowerRepository flowerRepository;
-    public String writeLetter(FlowerRequest request, UUID id) {
-        Flower flower = new Flower().builder()
-                .letter(request.getLetter())
-                .build();
-        return "test";
+    final private UserRepository userRepository;
+    final private CardItemService cardItemService;
+    final private BouquetRepository bouquetRepository;
+    final private FlowerItemService flowerItemService;
+    final private BouquetService bouquetService;
+    @Transactional
+    public String writeLetter(FlowerRequest request, UUID senderId, UUID ReceiverId) {
+        try {
+            User sender = userRepository.findByUserId(senderId).orElseThrow(()->new UserNotFoundException());
+            User receiver = userRepository.findByUserId(ReceiverId).get();
+            FlowerType flowerType = flowerItemService.minusFlowerItem(request, sender);
+            CardType cardType = cardItemService.minusCardItem(request, sender);
+            List<Flower> flowers = flowerRepository.findAllByReceiver(receiver);
+            Bouquet bouquet;
+
+            if (flowers.size() % 10 == 0) {
+                bouquet = bouquetService.createNewBouquet(receiver);
+            } else {
+                List<Bouquet> bouquets = bouquetRepository.findAllByUser(receiver);
+                bouquet = bouquets.get(bouquets.size() - 1);
+            }
+
+            Flower flower = new Flower().builder()
+                    .letter(request.getLetter())
+                    .flowerType(flowerType)
+                    .cardType(cardType)
+                    .sender(sender)
+                    .receiver(receiver)
+                    .bouquetId(bouquet)
+                    .build();
+            flowerRepository.save(flower);
+
+            return "저장에 성공하였습니다.";
+        } catch (ServiceException e) {
+            throw e;
+        }
     }
 }
+
