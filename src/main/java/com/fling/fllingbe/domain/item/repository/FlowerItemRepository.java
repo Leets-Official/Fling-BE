c8ae03d478590560eaba4327527718d8a1f68abc
rootTiket <l20078829@gmail.com>
2024-01-10 16:35:31 +0900

    :sparkles: [feat]: 유저와 아이템 id를 통해 유저가 가지고 있는 아이템을 가져오는 메소드 추가

--- src/main/java/com/fling/fllingbe/domain/item/repository/FlowerItemRepository.java
+++ src/main/java/com/fling/fllingbe/domain/item/repository/FlowerItemRepository.java
@@ -1,6 +1,7 @@
 package com.fling.fllingbe.domain.item.repository;
 
 import com.fling.fllingbe.domain.item.domain.FlowerItem;
+import com.fling.fllingbe.domain.item.domain.FlowerType;
 import com.fling.fllingbe.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,8 +9,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FlowerItemRepository extends JpaRepository<FlowerItem, Long> {
-    Optional<FlowerItem>
-    findById(Long flowerItemId);
+    Optional<FlowerItem> findById(Long flowerItemId);
     List<FlowerItem> findAllByUser(User user);
     Optional<FlowerItem> findByUser(User user);
+    Optional<FlowerItem> findByUserAndFlowerType(User user,FlowerType flowerType);
 }
