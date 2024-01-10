6ef7f03aaf8ba7094145cd8cb148301365d6ce32
rootTiket <l20078829@gmail.com>
2024-01-10 16:36:54 +0900

    :sparkles: [feat]: 유저가 가지고 있는 모든 꽃다발을 리스트로 가져오는 메서드 추가

--- src/main/java/com/fling/fllingbe/domain/bouquet/repository/BouquetRepository.java
+++ src/main/java/com/fling/fllingbe/domain/bouquet/repository/BouquetRepository.java
@@ -4,9 +4,11 @@ import com.fling.fllingbe.domain.bouquet.domain.Bouquet;
 import com.fling.fllingbe.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BouquetRepository extends JpaRepository<Bouquet,Long> {
     Optional<Bouquet> findById (Long bouquetId);
     Optional<Bouquet> findByUser(User user);
+    List<Bouquet> findAllByUser(User user);
 }
