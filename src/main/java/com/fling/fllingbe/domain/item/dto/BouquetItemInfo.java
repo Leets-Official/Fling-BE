4e5b15fd698d5abc5a9b0ece0518dd99770114a1
rootTiket <l20078829@gmail.com>
2024-01-15 11:34:22 +0900

    :sparkles: [feat]: 전체 리본과 포장지를 가져오는 로직 구현

---
+++ src/main/java/com/fling/fllingbe/domain/item/dto/BouquetItemInfo.java
@@ -0,0 +1,14 @@
+package com.fling.fllingbe.domain.item.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class BouquetItemInfo {
+    private Long id;
+    private String type;
+}
