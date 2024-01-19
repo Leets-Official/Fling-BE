f5c1e43acc4f2ac5f86169359eaba060a9c9f427
leeseobin00 <leeseobin000709@gmail.com>
2024-01-19 22:22:54 +0900

    ✨[feat]: Store Controller Deco 구매 API 추가

--- src/main/java/com/fling/fllingbe/domain/store/presentation/StoreController.java
+++ src/main/java/com/fling/fllingbe/domain/store/presentation/StoreController.java
@@ -1,6 +1,8 @@
 package com.fling.fllingbe.domain.store.presentation;
 
+import com.fling.fllingbe.domain.item.application.DecoItemService;
 import com.fling.fllingbe.domain.store.application.StoreService;
+import com.fling.fllingbe.domain.store.dto.DecoPurchaseRequest;
 import com.fling.fllingbe.domain.store.dto.FlowerPurchaseRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,10 +20,17 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class StoreController {
     private final StoreService storeService;
+    private final DecoItemService decoItemService;
 
     @PostMapping("/flower")
     public ResponseEntity<Map<String, String>> purchaseFlower(Authentication authentication, @RequestBody FlowerPurchaseRequest request) {
         storeService.purchaseFlower(request, authentication.getName());
         return new ResponseEntity<>(Map.of("message", "꽃 구매를 성공했습니다."), HttpStatus.OK);
+    }
+
+    @PostMapping("/deco")
+    public ResponseEntity<Map<String, String>> purchaseDeco(Authentication authentication, @RequestBody DecoPurchaseRequest request) {
+        storeService.purchaseDeco(request, authentication.getName());
+        return new ResponseEntity<>(Map.of("message", "데코 구매를 성공했습니다."), HttpStatus.OK);
     }
 }
