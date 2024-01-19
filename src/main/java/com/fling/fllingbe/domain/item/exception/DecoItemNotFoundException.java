2db40ae07aaefb19dc77bf197d126720ff8b58d5
leeseobin00 <leeseobin000709@gmail.com>
2024-01-19 22:21:38 +0900

    ✨ [feat]: Deco Item Not Found Exception 생성

---
+++ src/main/java/com/fling/fllingbe/domain/item/exception/DecoItemNotFoundException.java
@@ -0,0 +1,10 @@
+package com.fling.fllingbe.domain.item.exception;
+
+import com.fling.fllingbe.global.error.ErrorCode;
+import com.fling.fllingbe.global.error.exception.ServiceException;
+
+public class DecoItemNotFoundException extends ServiceException {
+    public DecoItemNotFoundException() {
+        super(ErrorCode.DECO_ITEM_NOT_FOUND);
+    }
+}
