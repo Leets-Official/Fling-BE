4c7fc2f29805392d70991cb637d8b58fbbc8445f
leeseobin00 <leeseobin000709@gmail.com>
2024-01-19 22:57:27 +0900

    ✨ [feat]: Card Type Not Found Exception 생성

---
+++ src/main/java/com/fling/fllingbe/domain/item/exception/CardTypeNotFoundException.java
@@ -0,0 +1,10 @@
+package com.fling.fllingbe.domain.item.exception;
+
+import com.fling.fllingbe.global.error.ErrorCode;
+import com.fling.fllingbe.global.error.exception.ServiceException;
+
+public class CardTypeNotFoundException extends ServiceException {
+    public CardTypeNotFoundException() {
+        super(ErrorCode.CARD_TYPE_NOT_FOUND);
+    }
+}
