0609aaddf19df27cdff9d071f29504cd061c1064
DongHoon Lee <dhl1924@naver.com>
2024-03-01 00:31:36 +0900

    test: pr builder failed test

--- src/main/java/com/fling/fllingbe/FlingApplication.java
+++ src/main/java/com/fling/fllingbe/FlingApplication.java
@@ -9,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class FlingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FlingApplication.class, d);
+        SpringApplication.run(FlingApplication.class, dd);
     }
 
 }
